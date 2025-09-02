a8fe1d1a913abab0437133b969f5eccb83627074
aaaaiii11 <2277134573@qq.com>
2025-09-02 10:00:06 +0800

    ok

--- myAi/src/App.jsx
+++ myAi/src/App.jsx
@@ -1,30 +1,11 @@
 import { OpenAI } from 'openai';
-// import { useReducer } from 'react';
 import { Bubble } from "@ant-design/x";
 import { Button, Input, Space,Card } from 'antd';
+import { useState }from 'react';
 
-// const reducer=(state,action)=>{ 
-//   switch(action.type){
-//     case 'user':{
-//       return{
-//         ...state,
-//         results:[...state.results,{content:action.value,role:'user'}]
-//       };
-//     }
-//     case 'ai':{
-//       return{
-//         ...state,
-//         results:[...state.results,{content:action.value,role:'ai'}]
-//       };
-//     }
-//   }
-// }
 const App= ()=>{
   const {Search}=Input;
-  // const [state,dispatch]=useReducer(reducer,{ results:[],})
-  // const { results }= state;
-  const messages=[];
-
+  const [messages,setMessages]=useState([]);
   const openai = new OpenAI({
     baseURL: 'https://api.deepseek.com',
     apiKey: 'sk-5aaf786676194a7b8c140b5f7ea00f8a',
@@ -32,43 +13,31 @@ const App= ()=>{
   });
   
   async function main(question) {
-    messages.push({"role":'user',"content":question})
+    const newMessages = [...messages,{"role":'user',"content":question}];
+    setMessages(newMessages);
     const completion = await openai.chat.completions.create({
-      messages:messages,
+      messages:newMessages,
       model: "deepseek-chat",
-      
     });
-    messages.push(completion.choices[0].message);
+    setMessages([...newMessages,completion.choices[0].message]);
     return completion.choices[0].message.content;
   }
 
 //将用户输入的问题保存起来
-  const onsearch = async (value, _e,info) => {
-    // dispatch({
-    //   type: 'user',
-    //   value: value
-    // })
-    // info = await main(value);
-    // dispatch({
-    //   type: 'ai',
-    //   value: info
-    // })
-    // console.log(messages)
-    info =await main(value);
-    console.log(info)
+  const onsearch = async (value) => {
+   await main(value);
     console.log(messages)
   }
 
-  
   return (
     <>
       <Card style={{ width: 900 }}>
         <Space direction="vertical">
-          {messages.map(result => {
-            if (result.role === 'assistant') {
-              return <Bubble style={{ width: 400 }} placement="start" content={result.content}  />
+          {messages.map(message => {
+            if (message.role === 'assistant') {
+              return <Bubble style={{ width: 400 }} placement="start" content={message.content}  />
             } else {
-              return <Bubble style={{ width: 400 }} placement="end" content={result.content} />
+              return <Bubble style={{ width: 400 }} placement="end" content={message.content} />
             }
           })}
         </Space>
@@ -76,7 +45,8 @@ const App= ()=>{
       <Search
         placeholder="input search loading with enterButton"
         style={{ width: 200 }}
-        onSearch={onsearch} >
+        onSearch={onsearch}
+        >
       </Search>
     </>
   )
