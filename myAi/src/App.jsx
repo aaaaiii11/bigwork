74ede28cb6f6070e61a8a2ac7fba4d9b6a289b5b
aaaaiii11 <2277134573@qq.com>
2025-09-02 09:59:22 +0800

    new

--- myAi/src/App.jsx
+++ myAi/src/App.jsx
@@ -1,28 +1,28 @@
 import { OpenAI } from 'openai';
-import { useReducer } from 'react';
+// import { useReducer } from 'react';
 import { Bubble } from "@ant-design/x";
 import { Button, Input, Space,Card } from 'antd';
 
-const reducer=(state,action)=>{ 
-  switch(action.type){
-    case 'user':{
-      return{
-        ...state,
-        results:[...state.results,{content:action.value,role:'user'}]
-      };
-    }
-    case 'ai':{
-      return{
-        ...state,
-        results:[...state.results,{content:action.value,role:'ai'}]
-      };
-    }
-  }
-}
+// const reducer=(state,action)=>{ 
+//   switch(action.type){
+//     case 'user':{
+//       return{
+//         ...state,
+//         results:[...state.results,{content:action.value,role:'user'}]
+//       };
+//     }
+//     case 'ai':{
+//       return{
+//         ...state,
+//         results:[...state.results,{content:action.value,role:'ai'}]
+//       };
+//     }
+//   }
+// }
 const App= ()=>{
   const {Search}=Input;
-  const [state,dispatch]=useReducer(reducer,{ results:[],})
-  const { results }= state;
+  // const [state,dispatch]=useReducer(reducer,{ results:[],})
+  // const { results }= state;
   const messages=[];
 
   const openai = new OpenAI({
@@ -44,15 +44,18 @@ const App= ()=>{
 
 //将用户输入的问题保存起来
   const onsearch = async (value, _e,info) => {
-    dispatch({
-      type: 'user',
-      value: value
-    })
-    info = await main(value);
-    dispatch({
-      type: 'ai',
-      value: info
-    })
+    // dispatch({
+    //   type: 'user',
+    //   value: value
+    // })
+    // info = await main(value);
+    // dispatch({
+    //   type: 'ai',
+    //   value: info
+    // })
+    // console.log(messages)
+    info =await main(value);
+    console.log(info)
     console.log(messages)
   }
 
@@ -61,8 +64,8 @@ const App= ()=>{
     <>
       <Card style={{ width: 900 }}>
         <Space direction="vertical">
-          {results.map(result => {
-            if (result.role === 'ai') {
+          {messages.map(result => {
+            if (result.role === 'assistant') {
               return <Bubble style={{ width: 400 }} placement="start" content={result.content}  />
             } else {
               return <Bubble style={{ width: 400 }} placement="end" content={result.content} />
